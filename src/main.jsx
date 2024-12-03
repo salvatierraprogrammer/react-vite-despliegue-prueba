6ab03ab8d941c5098940294d8b5b704e446641b0
salvatierraprogrammer <salvatierraprogrammer@gmail.com>
2024-12-03 03:15:51 -0300

    first commit2

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Note the change in import
+import ReactDOM from 'react-dom/client'; // Nota el cambio en la importación
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importa los estilos de Bootstrap
 
-const root = ReactDOM.createRoot(document.getElementById('root')); // Use createRoot
+// Crea un root para React y lo vincula al elemento con id 'root' en el HTML
+const root = ReactDOM.createRoot(document.getElementById('root')); // Usa createRoot
 root.render(
   <React.StrictMode>
     <App />
